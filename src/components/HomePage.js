c22e092dac2dc834756f79d3234b5cc510536519
Kristina Andersson <kristina.andersson@mail.ru>
2020-02-28 11:35:03 +0100

    style: moved background image to CSS

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,10 @@
 import React from "react";
-import img from "./coral-reef.jpg";
-import logo from "../../digitala-karriarrevet.png";
+import logo from "../digitala-karriarrevet.png";
 
 const HomePage = () => {
 	return (
 		<div className="App-content App-home">
 			<img className="App-logo" src={logo} alt="logo" />
-			<img className="App-background-img" src={img} alt="coral reef" />
 		</div>
 	);
 };
