7253f1a416f9710ef4f68507340d0a38caf5bd54
Kristina Andersson <kristina.andersson@mail.ru>
2020-02-29 01:46:08 +0100

    style: container styling and button animation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,23 @@
 import React from "react";
+import logo from "../digitala-karriarrevet.svg";
+import scrollButton from "../scroll-down.svg";
 
 const HomePage = () => {
 	return (
-		<div className="App-container">
-			<div className="App-container-item">this is some stuff</div>
-			<div className="App-container-item">this is some other stuff</div>
-			<div className="App-container-item">this is some stuff</div>
-			<div className="App-container-item">this is some other stuff</div>
-			<div className="App-container-item">this is some other stuff</div>
+		<div className="start-container">
+			<img className="App-logo" src={logo} alt="logo" />
+			<div className="start-info">
+				<div className="start-info__section">
+					<b>April 2020</b>
+				</div>
+				<div> |</div>
+				<div className="start-info__section">
+					Arrangerat av <b>NLG</b>
+				</div>
+			</div>
+			<div className="btn btn-icon">
+				<img src={scrollButton} alt="scroll button" />
+			</div>
 		</div>
 	);
 };
