b67d6552a89e66e945f42373c004708a5dd2d6c2
Kristina Andersson <kristina.andersson@mail.ru>
2020-04-05 21:13:01 +0200

    style: some overall styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,21 @@
 import React from "react";
 import logo from "../digitala-karriarrevet.svg";
 import Button from "./button/Button";
+import Header from "./Header";
 
 const HomePage = () => {
 	return (
-		<div className="start-container">
-			<img className="App-logo" src={logo} alt="logo" />
-			<div className="start-info">
-				<div className="start-info__section">
-					<Button special>Valbara kurser</Button>
-				</div>
-				<div className="start-info__section">
-					<Button special>Masterprogrammen</Button>
+		<div className="container overlay">
+			<Header></Header>
+			<div className="start-container">
+				<img className="App-logo" src={logo} alt="logo" />
+				<div className="start-info">
+					<div className="start-info__section">
+						<Button special>Valbara kurser</Button>
+					</div>
+					<div className="start-info__section">
+						<Button special>Masterprogrammen</Button>
+					</div>
 				</div>
 			</div>
 		</div>
