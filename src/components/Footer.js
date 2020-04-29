d7eda0105c3a7dab4d72d3c212f1d72276f14b5c
Kristina Andersson <kristina.andersson@mail.ru>
2020-04-29 15:30:09 +0200

    refactor: some changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from "react";
-import logo from "../digitala-karriarrevet.svg";
 
 const Footer = () => {
 	return (
 		<div className="App-footer">
-			<img className="App-logo" src={logo} alt="logo" />
+			<span>
+				Developed by{" "}
+				<a href="https://www.linkedin.com/in/kristina-andersson/">
+					Kristina Andersson
+				</a>{" "}
+				and NLG |{" "}
+				<a href="http://www.medieteknik.com/">Sektionen för Medieteknik</a>,{" "}
+				<a href="https://www.kth.se/">KTH</a>
+			</span>
 		</div>
 	);
 };
