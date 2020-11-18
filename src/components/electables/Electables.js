aeba6e3df55601c285b16f2e948223edd0122b93
Kristina Andersson <kristinaandersson@Kristinas-MacBook-Pro-2.local>
2020-11-18 16:58:02 +0100

    style: fix flex layout of buttons

--- src/components/electables/Electables.js
+++ src/components/electables/Electables.js
@@ -1,40 +1,38 @@
-import React from "react";
-import Button from "../button/Button";
-import { courses, questions } from "../../data/electables.json";
-import Course from "./Course";
+import React from 'react';
+import Button from '../button/Button';
+import { courses, questions } from '../../data/electables.json';
+import Course from './Course';
 
 const Electables = ({ history }) => {
 	return (
-		<div className="container" id="#electables">
-			<div className="start-container">
+		<div className='container' id='#electables'>
+			<div className='start-container'>
 				{/* <img className="App-logo" src={logo} alt="logo" /> */}
 				<h2>Valbara kurser</h2>
-				<div className="start-info">
-					<div className="start-info__section">
-						<a href="#DH2642">
+				<div className='start-info'>
+					<div className='start-info__section'>
+						<a href='#DH2642'>
 							<Button special>iProg:EN</Button>
 						</a>
 					</div>
-					<div className="start-info__section">
-						<a href="#DM2518">
+					<div className='start-info__section'>
+						<a href='#DM2518'>
 							<Button special>Mobilutvecklingen</Button>
 						</a>
 					</div>
-					<div className="start-info__section">
-						<a href="#DD2352">
+					<div className='start-info__section'>
+						<a href='#DD2352'>
 							<Button special>Algkomp:en</Button>
 						</a>
 					</div>
-				</div>
-				<div className="start-info">
-					<div className="start-info__section">
-						<a href="#SF1662">
+					<div className='start-info__section'>
+						<a href='#SF1662'>
 							<Button special>Diskmatten</Button>
 						</a>
 					</div>
 
-					<div className="start-info__section">
-						<a href="#DD1385">
+					<div className='start-info__section'>
+						<a href='#DD1385'>
 							<Button special>prutten</Button>
 						</a>
 					</div>
