f06e1032913763ee1f2cc4e76b0ddbfecfb7ac16
Kristina Andersson <kristinaandersson@Kristinas-MacBook-Pro-2.local>
2020-11-19 14:45:12 +0100

    style: minor improvments on responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "./button/Button";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './button/Button';
 
 const Header = () => {
 	return (
-		<div className="App-header" id="header">
-			<Link to="/">
-				<Button type="tertiary" id="Home-btn">
+		<div className='App-header' id='header'>
+			<Link to='/'>
+				<Button type='tertiary' id='Home-btn'>
 					hem
 				</Button>
 			</Link>
-			<div className="nav-bar">
-				<Link to="/about">
-					<Button type="tertiary" id="About-btn">
-						om karriärrevet
-					</Button>
-				</Link>
-			</div>
+			<Link to='/about'>
+				<Button type='tertiary' id='About-btn'>
+					om karriärrevet
+				</Button>
+			</Link>
 		</div>
 	);
 };
